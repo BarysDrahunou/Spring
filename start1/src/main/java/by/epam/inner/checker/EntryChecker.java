13c410d50e73da955c96d3dfecd479ddffbce0cd
Barys Drahunou <barysdrahunou@gmail.com>
2020-07-15 11:58:13 +0300

    delete start1

--- start1/src/main/java/by/epam/inner/checker/EntryChecker.java
+++
@@ -1,9 +0,0 @@
-package by.epam.inner.checker;
-
-import java.util.Map;
-
-public interface EntryChecker<K, V> {
-    boolean check(Map.Entry<K, V> entry);
-}
-
-
--- start1/src/main/java/by/epam/inner/date/DaysOfWeek.java
+++
@@ -1,5 +0,0 @@
-package by.epam.inner.date;
-
-public enum DaysOfWeek {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-}
